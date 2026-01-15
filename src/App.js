32a09ebffaa880f41c97081a64eb893e2273f89c
AleksandrKompanetc <moneymacker7@gmail.com>
2026-01-15 16:25:05 +0200

    added styles for app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className='app-wrapper'>
       <Header />
       <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        IMG
+      </div>
+      <div>
+        AVA + Description
+      </div>
+      <div>
+        My posts
+      </div>
+    </div>
+
+  )
+}
+
+export default Profile;

9aafd12ba79e019bc3e9c044640b6403bdc6a714
AleksandrKompanetc <moneymacker7@gmail.com>
2026-01-31 23:41:39 +0200

    created DialogItems and Message components

---
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink as Navlink } from "react-router-dom";
+
+const DialogItem = ( props ) => {
+  let path = '/dialogs/' + props.id;
+
+  return (
+    <div className={styles.dialog + ' ' + styles.active}>
+      <Navlink to={path}>{props.name}</Navlink>
+    </div>
+  );
+};
+
+export default DialogItem;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,7 @@
 import React from "react";
-import { NavLink as Navlink } from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 import styles from "./Dialogs.module.css";
-
-const DialogItem = ( props ) => {
-  let path = '/dialogs/' + props.id;
-
-  return (
-    <div className={styles.dialog + ' ' + styles.active}>
-      <Navlink to={path}>{props.name}</Navlink>
-    </div>
-  );
-};
-
-const Message = ( props ) => {
-  return (
-    <div className={styles.dialog}>{props.message}</div>
-  )
-}
 
 const Dialogs = () => {
 

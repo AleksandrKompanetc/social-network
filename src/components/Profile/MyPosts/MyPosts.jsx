a41c16c002b25598fe9de58bdf7d7be91eead550
AleksandrKompanetc <moneymacker7@gmail.com>
2026-02-06 15:09:22 +0200

    created function addPost and using ref

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,14 +4,13 @@ import styles from './MyPosts.module.css';
 
 const MyPosts = (props) => {
 
-  // let posts = [
-  //   { id: 1, message: "Hi, how are you?", likesCount: 5 },
-  //   { id: 2, message: "It's my first post", likesCount: 10 },
-  //   { id: 3, message: "Hello world", likesCount: 11 },
-  //   { id: 4, message: "New post", likesCount: 11 }
-  // ];
-
   let postsElements = props.posts.map(post => <Post message={post.message} likesCount={post.likesCount} />);
+
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+  }
 
   return (
     <div className={styles.postsBlock}>
@@ -19,10 +18,10 @@ const MyPosts = (props) => {
       <div>
         New posts
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add Post</button>
+          <button onClick={addPost}>Add Post</button>
         </div>
       </div>
       <div className={styles.posts}>

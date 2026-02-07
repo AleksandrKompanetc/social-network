5a9bd90157b2c8d818b91117f90f48becd1f5be5
AleksandrKompanetc <moneymacker7@gmail.com>
2026-02-07 21:54:55 +0200

    created addPost function

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,15 +5,26 @@ import styles from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
 
+  let newMessage = React.createRef();
+
   let dialogsElements = props.state.dialogs.map( dialog => <DialogItem name={dialog.name} id={dialog.id} />)
   let messagesElements = props.state.messages.map( message => <Message message={message.message} />)
+
+  let addMessage = () => {
+    let text = newMessage.current.value;
+  }
 
   return (
     <div className={styles.dialogs}>
       <div className={styles.dialogsItems}>
         {dialogsElements}
       </div>
+
       <div className={styles.messages}>
+        <div>
+          <textarea ref={newMessage}></textarea>
+          <button onClick={addMessage}>Add message</button>
+        </div>
         {messagesElements}
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state.js';
+import { addPost } from './redux/state.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+addPost("Samurai Jack");
 root.render(
   <React.StrictMode>
     <App state={state} />

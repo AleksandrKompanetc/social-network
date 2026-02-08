81a84014b277068216234994cf0d81245230c7e5
AleksandrKompanetc <moneymacker7@gmail.com>
2026-02-08 19:57:57 +0200

    created function rerenderEntireTree to redraw the state

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import state from './redux/state.js';
 import { addPost } from './redux/state.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-addPost("Samurai Jack");
-root.render(
-  <React.StrictMode>
-    <App state={state} />
-  </React.StrictMode>
-);
+let rerenderEntireTree = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
 
+  root.render(
+    <React.StrictMode>
+      <App state={state} addPost={addPost} />
+    </React.StrictMode>
+  );
+}
+
+rerenderEntireTree();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

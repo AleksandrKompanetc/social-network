17ce95ed0e0f9564ad522bac56a45d262733fa98
AleksandrKompanetc <moneymacker7@gmail.com>
2026-02-14 11:05:01 +0200

    added dispatch

--- src/redux/state.js
+++ src/redux/state.js
@@ -25,30 +25,33 @@ let store = {
     },
     sidebar: {}
   },
-  getState() {
-    return this._state;
-  },
   _callSubscriber() {
     console.log('State');
   },
-  addPost() {
-  let newPost = {
-    id: 5,
-    message: this._state.profilePage.newPostText,
-    likesCount: 0
-  }
-
-  this._state.profilePage.posts.push(newPost);
-  this._state.profilePage.newPostText = '';
-  this._callSubscriber(this._state);
-},
-  updateNewPostText(newText) {
-  this._state.profilePage.newPostText = newText;
-  this._callSubscriber(this._state);
-},
+  getState() {
+    return this._state;
+  },
   subscribe(observer) {
-  this._callSubscriber = observer;
-}
+    this._callSubscriber = observer;
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0
+    }
+
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  dispatch() {
+
+  }
 }
 
 window.store = store;

b9e5cf69df8df40e108a725533d750fe39fe5595
AleksandrKompanetc <moneymacker7@gmail.com>
2026-02-19 23:58:01 +0200

    changed dispatch for sending message

--- src/redux/state.js
+++ src/redux/state.js
@@ -57,6 +57,11 @@ let store = {
     } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
       this._state.dialogsPage.newMessageBody = action.body;
       this._callSubscriber(this._state);
+    } else if (action.type === SEND_MESSAGE) {
+      let body = this._state.dialogsPage.newMessageBody;
+      this._state.dialogsPage.newMessageBody = '';
+      this._state.dialogsPage.messages.push({ id: 4, message: body });
+      this._callSubscriber(this._state);
     }
   }
 }
